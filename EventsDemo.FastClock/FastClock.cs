8f6924a02d962058d7ce9b251fa2efa9ff4ff591
maHlavacek <martin_hlavacek@icloud.com>
2018-12-13 09:50:49 +0100

    FirstTry

---
+++ EventsDemo.FastClock/FastClock.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows.Threading;
+
+namespace EventsDemo.FastClock
+{
+    public class FastClock
+    {
+        #region Fields
+
+        public static FastClock _instance;
+        private DispatcherTimer _dispatcherTimer;
+
+        private event EventHandler<DateTime> OneMinuteIsOver;
+
+        #endregion
+
+
+        #region Properties
+
+        public int Factor { get; set; }
+        public static FastClock Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new FastClock();
+                }
+                return _instance;
+            } 
+        }
+        public bool IsRunning { get; set; }
+        public DateTime Time { get; set; }
+
+
+        #endregion
+
+        #region Constructor
+
+        private FastClock()
+        {
+            _dispatcherTimer = new DispatcherTimer();
+            _dispatcherTimer.Tick += Timer_Tick;
+            _instance.OneMinuteIsOver += OnOneMinuteIsOver;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void OnOneMinuteIsOver(object sender, DateTime time)
+        {
+            while (IsRunning)
+            {
+                OneMinuteIsOver?.Invoke(this, time);
+            }
+        }
+
+        private void Timer_Tick(object sender,EventArgs args)
+        {
+            _dispatcherTimer.Interval = new TimeSpan(0,0,Factor);
+            _dispatcherTimer.Start();
+        }
+        #endregion
+    }
+}
---
+++ EventsDemo.FastClockWpf/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+
+namespace EventsDemo.FastClockWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MetroWindow_Initialized(object sender, EventArgs e)
+        {
+            DatePickerDate.SelectedDate = DateTime.Today;
+            TextBoxTime.Text = DateTime.Now.ToShortTimeString();
+        }
+
+        private void ButtonSetTime_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlockDate.Text = DatePickerDate.Text;
+            TextBlockTime.Text = TextBoxTime.Text;
+        }
+
+        private void SetFastClockStartDateAndTime()
+        {
+            FastClock.FastClock.Instance.Time = DateTime.Parse(TextBlockDate.Text +" "+ TextBlockTime);
+        }
+
+        private void FastClockOneMinuteIsOver(object sender, DateTime fastClockTime)
+        {
+           
+        }
+
+        private void CheckBoxClockRuns_Click(object sender, RoutedEventArgs e)
+        {
+            if(CheckBoxClockRuns.IsChecked.Value)
+            {
+                FastClock.FastClock.Instance.IsRunning = true;
+
+                if (int.TryParse(TextBoxFactor.Text, out int factor))
+                {
+                    FastClock.FastClock.Instance.Factor = factor;
+                }
+                else
+                {
+                    TextBoxFactor.Text = FastClock.FastClock.Instance.Factor.ToString();
+                }              
+            }
+            FastClock.FastClock.Instance.IsRunning = false;
+        }
+
+        private void ButtonCreateView_Click(object sender, RoutedEventArgs e)
+        {
+            DigitalClock digitalClock = new DigitalClock();
+            digitalClock.Owner = this;
+            digitalClock.Show();
+        }
+    }
+}
